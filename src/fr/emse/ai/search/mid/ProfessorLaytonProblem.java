bcf584d71de5224d9b61f007868fe900cdaf9775
Blackcreed <elouanderenne@gmail.com>
2022-03-17 15:43:04 +0100

    layton puzzle

--- src/fr/emse/ai/search/mid/ProfessorLaytonProblem.java
+++ src/fr/emse/ai/search/mid/ProfessorLaytonProblem.java
@@ -7,8 +7,8 @@ import java.util.Collection;
 
 public class ProfessorLaytonProblem implements Problem {
 
-    SimpleState initialState = new SimpleState(3,3,0,0, true);
-    SimpleState finalState = new SimpleState(0,0,3,3, false);
+    SimpleState initialState = new SimpleState(3,0,3,0, true);
+    SimpleState finalState = new SimpleState(0,3,0,3, false);
 
     @Override
     public Object getInitialState() {
@@ -24,16 +24,58 @@ public class ProfessorLaytonProblem implements Problem {
     public Collection<Object> getActions(Object state) {
         ArrayList<Object> actions = new ArrayList<Object>();
         int cannibalLeft = ((SimpleState)state).cannibalLeft;
-        int cannibalLeft = ((SimpleState)state).cannibalLeft;
-        int cannibalLeft = ((SimpleState)state).cannibalLeft;
-        int cannibalLeft = ((SimpleState)state).cannibalLeft;
-        int cannibalLeft = ((SimpleState)state).cannibalLeft;
+        int cannibalRight = ((SimpleState)state).cannibalRight;
+        int missionariesLeft = ((SimpleState)state).missionariesLeft;
+        int missionariesRight = ((SimpleState)state).missionariesRight;
+        boolean boatOnLeftSide = ((SimpleState)state).boatOnLeftSide;
 
-        return null;
+        if(boatOnLeftSide){
+            if(cannibalLeft>=2){
+                actions.add("take 2 cannibals Left to Right");
+                actions.add("take 1 cannibals Left to Right");
+                if(missionariesLeft>=1){
+                    actions.add("take 1 each Left to Right");
+                }
+            }
+
+            if(missionariesLeft >=2){
+                actions.add("take 2 missionaries Left to Right");
+                actions.add("take 1 missionaries Left to Right");
+            }
+        } else {
+            if(cannibalRight>=2){
+                actions.add("take 2 cannibals Right to Left");
+                actions.add("take 1 cannibals Right to Left");
+                if(missionariesRight>=1){
+                    actions.add("take 1 each Right to Left");
+                }
+            }
+
+            if(missionariesRight >=2){
+                actions.add("take 2 missionaries Right to Left");
+                actions.add("take 1 missionaries Right to Left");
+            }
+        }
+        return actions;
     }
 
     @Override
     public Object getNextState(Object state, Object action) {
+        int cannibalLeft = ((SimpleState)state).cannibalLeft;
+        int cannibalRight = ((SimpleState)state).cannibalRight;
+        int missionariesLeft = ((SimpleState)state).missionariesLeft;
+        int missionariesRight = ((SimpleState)state).missionariesRight;
+        boolean boatOnLeftSide = ((SimpleState)state).boatOnLeftSide;
+        if (action.equals("take 2 cannibals Left to Right")) return new SimpleState(cannibalLeft-2, cannibalRight+2, missionariesLeft, missionariesRight, !boatOnLeftSide);
+        if (action.equals("take 1 cannibals Left to Right")) return new SimpleState(cannibalLeft-1, cannibalRight+1, missionariesLeft, missionariesRight, !boatOnLeftSide);
+        if (action.equals("take 1 each Left to Right")) return new SimpleState(cannibalLeft-1, cannibalRight+1, missionariesLeft-1, missionariesRight+1, !boatOnLeftSide);
+        if (action.equals("take 2 missionaries Left to Right") && (cannibalLeft<missionariesLeft-2)) return new SimpleState(cannibalLeft, cannibalRight, missionariesLeft-2, missionariesRight+2, !boatOnLeftSide);
+        if (action.equals("take 1 missionaries Left to Right") && (cannibalLeft<missionariesLeft-1)) return new SimpleState(cannibalLeft, cannibalRight, missionariesLeft-1, missionariesRight+1, !boatOnLeftSide);
+        if (action.equals("take 2 cannibals Right to Left")) return new SimpleState(cannibalLeft+2, cannibalRight-2, missionariesLeft, missionariesRight, !boatOnLeftSide);
+        if (action.equals("take 1 cannibals Right to Left")) return new SimpleState(cannibalLeft+1, cannibalRight-1, missionariesLeft, missionariesRight, !boatOnLeftSide);
+        if (action.equals("take 1 each Right to Left")) return new SimpleState(cannibalLeft+1, cannibalRight-1, missionariesLeft+1, missionariesRight-1, !boatOnLeftSide);
+        if (action.equals("take 2 missionaries Right to Left") && (cannibalRight<missionariesRight-2)) return new SimpleState(cannibalLeft, cannibalRight, missionariesLeft+2, missionariesRight-2, !boatOnLeftSide);
+        if (action.equals("take 1 missionaries Right to Left") && (cannibalRight<missionariesRight-1)) return new SimpleState(cannibalLeft, cannibalRight, missionariesLeft+1, missionariesRight-1, !boatOnLeftSide);
         return null;
     }
 
--- src/fr/emse/ai/search/simple/SimpleTest.java
+++ src/fr/emse/ai/search/simple/SimpleTest.java
@@ -1,5 +1,6 @@
 package fr.emse.ai.search.simple;
 
+import fr.emse.ai.search.mid.ProfessorLaytonProblem;
 import fr.emse.ai.search.solver.BreadthFirstGraphSearch;
 import fr.emse.ai.search.solver.BreadthFirstTreeSearch;
 import fr.emse.ai.search.solver.DepthFirstGraphSearch;
@@ -10,12 +11,15 @@ public class SimpleTest {
     public static void main(String[] args) {
         SimpleOrientedGraphProblem p1 = new SimpleOrientedGraphProblem();
         SimpleGraphProblem p2 = new SimpleGraphProblem();
+        ProfessorLaytonProblem p3 = new ProfessorLaytonProblem();
         System.out.println("Solution to problem using depth first : ");
         //System.out.println(new DepthFirstTreeSearch().solve(p1).pathToString());
         //System.out.println(new BreadthFirstTreeSearch().solve(p1).pathToString());
         //System.out.println(new DepthFirstTreeSearch().solve(p2).pathToString()); //Infinite Loop
         //System.out.println(new BreadthFirstTreeSearch().solve(p2).pathToString());
-        System.out.println(new DepthFirstGraphSearch().solve(p1).pathToString());
-        System.out.println(new BreadthFirstGraphSearch().solve(p1).pathToString());
+        //System.out.println(new DepthFirstGraphSearch().solve(p2).pathToString());
+        //System.out.println(new BreadthFirstGraphSearch().solve(p2).pathToString());
+        //System.out.println(new BreadthFirstGraphSearch().solve(p3).pathToString());
+        System.out.println(new DepthFirstGraphSearch().solve(p3).pathToString());
     }
 }
